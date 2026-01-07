0dbf3780bd6613d3459b0baf243bae1ac5ae3bae
Lynnette25 <frances.heredia@gmail.com>
2026-01-07 16:01:19 -0400

    Refactor  MET_df for peakset analysis

--- ATAC-seq_Processing/Differential_analisis_Diffbind.R
+++ ATAC-seq_Processing/Differential_analisis_Diffbind.R
@@ -11,8 +11,8 @@
 library(DiffBind)
 
 ### Load the peak files from MACS2 to DiffBind
-# Initialize the SKOV3 object with the first peakset
-SKOV3 <- dba.peakset(NULL, 
+# Initialize the MET_df object with the first peakset
+MET_df <- dba.peakset(NULL, 
                      peaks="A8BR_1/peak_calling_hg38/A8BR_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A8BR_1", 
@@ -21,7 +21,7 @@ SKOV3 <- dba.peakset(NULL,
                      bamReads="A8BR_1/aligned_hg38/A8BR_1_sort_dedup.bam")
 
 # Add the second replicate for A8BR
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A8BR_2/peak_calling_hg38/A8BR_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A8BR_2", 
@@ -30,7 +30,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      bamReads="A8BR_2/aligned_hg38/A8BR_2_sort_dedup.bam")
 
 # Add the first replicate for A49BR
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A49BR_1/peak_calling_hg38/A49BR_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A49BR_1", 
@@ -39,7 +39,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      bamReads="A49BR_1/aligned_hg38/A49BR_1_sort_dedup.bam")
 
 # Add the second replicate for A49BR
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A49BR_2/peak_calling_hg38/A49BR_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A49BR_2", 
@@ -48,7 +48,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      bamReads="A49BR_2/aligned_hg38/A49BR_2_sort_dedup.bam")
 
 # Add the first replicate for A39BR
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A39BR_1/peak_calling_hg38/A39BR_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A39BR_1", 
@@ -60,7 +60,7 @@ SKOV3 <- dba.peakset(SKOV3,
 
 # Add peak sets for the "Liver" condition with appropriate replicates
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A8LIV_1/peak_calling_hg38/A8LIV_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A8LIV_1", 
@@ -68,7 +68,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A8LIV_1/aligned_hg38/A8LIV_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A8LIV_2/peak_calling_hg38/A8LIV_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A8LIV_2", 
@@ -76,7 +76,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A8LIV_2/aligned_hg38/A8LIV_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A39LIV_1/peak_calling_hg38/A39LIV_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A39LIV_1", 
@@ -84,7 +84,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A39LIV_1/aligned_hg38/A39LIV_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A39LIV_2/peak_calling_hg38/A39LIV_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A39LIV_2", 
@@ -92,7 +92,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A39LIV_2/aligned_hg38/A39LIV_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A49LIV_1/peak_calling_hg38/A49LIV_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A49LIV_1", 
@@ -100,7 +100,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A49LIV_1/aligned_hg38/A49LIV_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A49LIV_2/peak_calling_hg38/A49LIV_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A49LIV_2", 
@@ -108,7 +108,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A49LIV_2/aligned_hg38/A49LIV_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A18LIV_1/peak_calling_hg38/A18LIV_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A18LIV_1", 
@@ -116,7 +116,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A18LIV_1/aligned_hg38/A18LIV_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A18LIV_2/peak_calling_hg38/A18LIV_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A18LIV_2", 
@@ -124,7 +124,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A18LIV_2/aligned_hg38/A18LIV_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A23LIV_1/peak_calling_hg38/A23LIV_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A23LIV_1", 
@@ -132,7 +132,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A23LIV_1/aligned_hg38/A23LIV_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A23LIV_2/peak_calling_hg38/A23LIV_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A23LIV_2", 
@@ -140,7 +140,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A23LIV_2/aligned_hg38/A23LIV_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A33LIV_1/peak_calling_hg38/A33LIV_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A33LIV_1", 
@@ -148,7 +148,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A33LIV_1/aligned_hg38/A33LIV_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A33LIV_2/peak_calling_hg38/A33LIV_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A33LIV_2", 
@@ -156,7 +156,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A33LIV_2/aligned_hg38/A33LIV_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A36LIV_1/peak_calling_hg38/A36LIV_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A36LIV_1", 
@@ -164,7 +164,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A36LIV_1/aligned_hg38/A36LIV_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A36LIV_2/peak_calling_hg38/A36LIV_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A36LIV_2", 
@@ -177,7 +177,7 @@ SKOV3 <- dba.peakset(SKOV3,
 
 # Add peak sets for the "Lung" condition with appropriate replicates
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A8LUN_1/peak_calling_hg38/A8LUN_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A8LUN_1", 
@@ -185,7 +185,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A8LUN_1/aligned_hg38/A8LUN_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A8LUN_2/peak_calling_hg38/A8LUN_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A8LUN_2", 
@@ -193,7 +193,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A8LUN_2/aligned_hg38/A8LUN_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A49LUN_1/peak_calling_hg38/A49LUN_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A49LUN_1", 
@@ -201,7 +201,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A49LUN_1/aligned_hg38/A49LUN_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A49LUN_2/peak_calling_hg38/A49LUN_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A49LUN_2", 
@@ -209,7 +209,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A49LUN_2/aligned_hg38/A49LUN_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A18LUN_1/peak_calling_hg38/A18LUN_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A18LUN_1", 
@@ -217,7 +217,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A18LUN_1/aligned_hg38/A18LUN_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A18LUN_2/peak_calling_hg38/A18LUN_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A18LUN_2", 
@@ -225,7 +225,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A18LUN_2/aligned_hg38/A18LUN_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A23LUN_1/peak_calling_hg38/A23LUN_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A23LUN_1", 
@@ -233,7 +233,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A23LUN_1/aligned_hg38/A23LUN_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A23LUN_2/peak_calling_hg38/A23LUN_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A23LUN_2", 
@@ -241,7 +241,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A23LUN_2/aligned_hg38/A23LUN_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A33LUN_1/peak_calling_hg38/A33LUN_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A33LUN_1", 
@@ -249,7 +249,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A33LUN_1/aligned_hg38/A33LUN_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A33LUN_2/peak_calling_hg38/A33LUN_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A33LUN_2", 
@@ -257,7 +257,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=2, 
                      bamReads="A33LUN_2/aligned_hg38/A33LUN_2_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A36LUN_1/peak_calling_hg38/A36LUN_1_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A36LUN_1", 
@@ -265,7 +265,7 @@ SKOV3 <- dba.peakset(SKOV3,
                      replicate=1, 
                      bamReads="A36LUN_1/aligned_hg38/A36LUN_1_sort_dedup.bam")
 
-SKOV3 <- dba.peakset(SKOV3, 
+MET_df <- dba.peakset(MET_df, 
                      peaks="A36LUN_2/peak_calling_hg38/A36LUN_2_peaks.xls", 
                      peak.caller="macs", 
                      sampID="A36LUN_2", 
@@ -278,43 +278,43 @@ SKOV3 <- dba.peakset(SKOV3,
 
 #### Affinity analysis - differential accessibility analysis
 cat("counts")
-SKOV3_counts<-dba.count(SKOV3, bParallel = TRUE, score=DBA_SCORE_READS, summits=TRUE)
-SKOV3_counts
+MET_df_counts<-dba.count(MET_df, bParallel = TRUE, score=DBA_SCORE_READS, summits=TRUE)
+MET_df_counts
 
 cat("normalize")
-SKOV3_normalize <- dba.normalize(SKOV3_counts, method=DBA_DESEQ2, normalize=DBA_NORM_NATIVE, library=DBA_LIBSIZE_BACKGROUND, background=TRUE)
-#counts<-dba.count(SKOV3)
+MET_df_normalize <- dba.normalize(MET_df_counts, method=DBA_DESEQ2, normalize=DBA_NORM_NATIVE, library=DBA_LIBSIZE_BACKGROUND, background=TRUE)
+#counts<-dba.count(MET_df)
 cat("contrast")
-counts<-dba.contrast(SKOV3_normalize, categories=DBA_CONDITION,minMembers = 2)
+counts<-dba.contrast(MET_df_normalize, categories=DBA_CONDITION,minMembers = 2)
 
 cat("analyzed")
 
-SKOV3_analyzed<-dba.analyze(counts)
+MET_df_analyzed<-dba.analyze(counts)
 cat("plots")
 
 png('BR_vs_MET/All_volcano_BR_vs_MET.png', units="in", width=7, heigh=7, res=800)
-dba.plotVolcano(SKOV3_analyzed)
+dba.plotVolcano(MET_df_analyzed)
 dev.off()
 
 png('BR_vs_MET/All_Heatmap_BR_vs_MET.png', units="in", width=7, heigh=7, res=800)
-dba.plotHeatmap(SKOV3_analyzed)
+dba.plotHeatmap(MET_df_analyzed)
 dev.off()
 
 png('BR_vs_MET/All_MA_BR_vs_MET.png', units="in", width=7, heigh=7, res=800)
-dba.plotMA(SKOV3_analyzed)
+dba.plotMA(MET_df_analyzed)
 dev.off()
 
 png('BR_vs_MET/All_PCA_BR_vs_MET.png', units="in", width=7, heigh=7, res=800)
-dba.plotPCA(SKOV3_analyzed,contrast=1,label=DBA_CONDITION)
+dba.plotPCA(MET_df_analyzed,contrast=1,label=DBA_CONDITION)
 dev.off()
 
-profiles <- dba.plotProfile(SKOV3_analyzed)
+profiles <- dba.plotProfile(MET_df_analyzed)
 png('BR_vs_MET/All_Profile_BR_vs_MET.png', units="in", width=7, heigh=7, res=800)
 dba.plotProfile(profiles)
 dev.off()
 
 
-SKOV3_DE_peaks <- dba.report(SKOV3_analyzed)
-SKOV3_DE_peaks_DF<-as.data.frame(SKOV3_DE_peaks)
+MET_df_DE_peaks <- dba.report(MET_df_analyzed)
+MET_df_DE_peaks_DF<-as.data.frame(MET_df_DE_peaks)
 # Filtering
-write.table(SKOV3_DE_peaks_DF[abs(SKOV3_DE_peaks_DF$Fold)>0.5 & SKOV3_DE_peaks_DF$FDR<0.05,], "BR_vs_MET/All_diff_regions_BR_vs_MET.txt", sep = "\t", quote = F)
+write.table(MET_df_DE_peaks_DF[abs(MET_df_DE_peaks_DF$Fold)>0.5 & MET_df_DE_peaks_DF$FDR<0.05,], "BR_vs_MET/All_diff_regions_BR_vs_MET.txt", sep = "\t", quote = F)
